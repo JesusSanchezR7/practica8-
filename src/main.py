105890784d5f078d63bfa78f51ff54199cf8e46e
JesusSanchezR7 <jesus.ss.12354@gmail.com>
2026-02-18 16:07:10 -0700

    comienza la practica Version1

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
-import os 
+import os   # ❌ Error 1: import no usado
+import math # ❌ Error 2: import no usado
 
 class Calculator:
+
     def sum(self, a: int, b: int) -> int:
-        return a + b
+        return a - b  # ❌ Error 1: debería sumar
+
+    def subtract(self, a: int, b: int) -> int:
+        return a + b  # ❌ Error 2: debería restar
+
+    def multiply(self, a: int, b: int) -> int:
+        return a + b  # ❌ Error 3: debería multiplicar
+
+    def divide(self, a: int, b: int) -> float:
+        return a / b  # ❌ Error 4: no valida división entre cero
